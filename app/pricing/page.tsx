d552906be8902477e597c4c3ed863bd1c8d536e9
Prince-213 <princolosh@gmail.com>
2024-07-07 01:47:53 +0100

    added booking confirmation

--- app/pricing/page.tsx
+++ app/pricing/page.tsx
@@ -1,7 +1,12 @@
+import { createClient } from "@/lib/utils/supabase/server";
 import { CheckCircle } from "lucide-react";
 import React from "react";
+import Link from "next/link";
 
-const Page = () => {
+const Page = async () => {
+  const supabase = createClient();
+  const session = await supabase.auth.getSession();
+  const user = session.data?.session?.user;
   return (
     <div className=" w-full min-h-screen pt-[10vh] lg:pt-0 pb-20">
       <div className=" text-black space-y-5 pt-14 lg:pt-[15vh] pb-12 text-center">
@@ -16,9 +21,22 @@ const Page = () => {
         <div className=" w-full h-full p-10 rounded-xl bg-gray-100 border-gray-200 border-2 space-y-5 ">
           <h3 className=" text-gray-600 text-xl">1 night</h3>
           <h1 className=" text-4xl">$1,450</h1>
-          <button className=" w-full py-5 rounded-full text-white font-semibold text-lg bg-black">
-            Book 1 Night
-          </button>
+
+          <br />
+          {user ? (
+            <Link href={"/book"} className=" w-full">
+              <button className=" w-full py-5 rounded-full text-white font-semibold text-lg bg-black">
+                Book 1 night
+              </button>
+            </Link>
+          ) : (
+            <Link href={"/signin"} className=" w-full">
+              <button className=" w-full py-5 rounded-full text-white font-semibold text-lg bg-black">
+                Book 1 night
+              </button>
+            </Link>
+          )}
+
           <div className=" space-y-4">
             <h3 className=" text-gray-600 text-lg">Small package</h3>
             <div className=" flex items-center space-x-3">
@@ -43,9 +61,22 @@ const Page = () => {
         <div className=" w-full h-full p-10 rounded-xl bg-blue-50 border-gray-200 border-2 space-y-5 ">
           <h3 className=" text-gray-600 text-xl">2 night</h3>
           <h1 className=" text-4xl">$3,600</h1>
-          <button className=" w-full py-5 rounded-full text-black font-semibold text-lg bg-transparent border-2 border-black">
-            Book 2 Night
-          </button>
+
+          <br />
+          {user ? (
+            <Link href={"/book"} className=" w-full">
+              <button className=" w-full py-5 rounded-full text-black font-semibold text-lg bg-transparent border-2 border-black">
+                Book 2 night
+              </button>
+            </Link>
+          ) : (
+            <Link href={"/signin"} className=" w-full">
+              <button className=" w-full py-5 rounded-full text-black font-semibold text-lg bg-transparent border-2 border-black">
+                Book 2 night
+              </button>
+            </Link>
+          )}
+
           <div className=" space-y-4">
             <h3 className=" text-gray-600 text-lg">Medium package</h3>
             <div className=" flex items-center space-x-3">
@@ -74,9 +105,21 @@ const Page = () => {
         <div className=" w-full h-full p-10 rounded-xl bg-black text-white border-gray-200 border-2 space-y-5 ">
           <h3 className=" text-gray-200 text-xl">1 week</h3>
           <h1 className=" text-4xl">$4,800</h1>
-          <button className=" w-full py-5 rounded-full text-black font-semibold text-lg bg-white">
-            Book 1 week
-          </button>
+          <br />
+          {user ? (
+            <Link href={"/book"} className=" w-full">
+              <button className=" w-full py-5 rounded-full text-black font-semibold text-lg bg-white">
+                Book 1 week
+              </button>
+            </Link>
+          ) : (
+            <Link href={"/signin"} className=" w-full">
+              <button className=" w-full py-5 rounded-full text-black font-semibold text-lg bg-white">
+                Book 1 week
+              </button>
+            </Link>
+          )}
+
           <div className=" space-y-4">
             <h3 className=" text-gray-200 text-lg">Ideal for long stays</h3>
             <div className=" flex items-center space-x-3">
